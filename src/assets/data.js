0c0bda57bed1f16605e63c16bce5df4280b073ee
Elena Blundell <blundell.elena@gmail.com>
2023-08-22 20:59:35 -0500

    added data.js file
    added data.js file for projects info

---
+++ src/assets/data.js
@@ -0,0 +1,12 @@
+import { faTicketSimple } from "@fortawesome/free-solid-svg-icons";
+
+export default [
+    {
+        id: 1;
+        title: ;
+        img: ;
+        description: ;
+        
+    }
+
+]
