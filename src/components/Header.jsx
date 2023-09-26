567c47253578bf203457f2868f8766ce81bb8c96
Elena Blundell <blundell.elena@gmail.com>
2023-09-26 16:48:27 -0500

    responsive navbar works now

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,37 @@
-import React from 'react'
+import React, {useState} from 'react'
 // import { Icon } from 'react-icons-kit'
 // import {menu} from 'react-icons-kit/feather/menu'
 // import {x} from 'react-icons-kit/feather/x'
 import '/src/components/Header.css'
 
 
+
 export default function Header(){
+
+    const [isOpen, setIsOpen] = useState(false)
+
+    const handleMenu = () => {
+        setIsOpen(!isOpen)
+    }
+
     return (
             <div>
-                <header className="header">
+                <header className={isOpen ? "header expanded" : "header"}>
                     <h1 className="logo">Elena Blundell</h1>
-                    <nav className="nav">
+                    <nav className={isOpen ? "nav-open" : "nav"}>
+                    <div className={isOpen ? "menu-icon open" : "menu-icon"} onClick={handleMenu}>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        {/* <Icon icon={menu} size={32} /> */}
+                        </div>
                         <ul>
                             <li tabIndex="-1"><a href="#about">About</a></li>
                             <li tabIndex="-1"><a href="#tech">Tech stack</a></li>
                             <li tabIndex="-1"><a href="#projects">Projects</a></li>
                             <li tabIndex="-1"><a href="#contacts">Contacts</a></li>
                         </ul>
-                        <div className="menu-icon">
-                            <span></span>
-                            <span></span>
-                            <span></span>
-                        {/* <Icon icon={menu} size={32} /> */}
-                        </div>
+                       
                     </nav>
                    
                 </header>
